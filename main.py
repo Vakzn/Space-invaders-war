f31322f70d7e761e0132769c826a1e09b7db70e4
Vasya Korghikov <vakzn@yandex.ru>
2021-06-20 16:44:43 +0300

    Close screen

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import pygame
 #иннициализируем pygame
 pygame.init()
 
+#создаем экран
 screen = pygame.display.set_mode((800, 600))
 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
